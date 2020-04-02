ebf39b8438725a102483b86288ff80eff44b6864
LucaEvaroni <info@luca.evaroni.name>
2020-04-02 15:35:57 +0200

    first commit

---
+++ DeVLearninG.Rx.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DeVLearninG.Rx.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            System.Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rx
+
+
+## Benvenuti su DeVLearninG
